c74bedf7b88bab1a403ffa4c35e8bf4add1eca3e
FamousCake <peter.g.hristov@gmail.com>
2015-02-09 10:40:13 +0000

    added push-relabel

---
+++ src/inc/push_relabel.h
@@ -0,0 +1,12 @@
+#ifndef PUSH_RELABEL_H
+#define PUSH_RELABEL_H
+
+class PushRelabel
+{
+public:
+    PushRelabel();
+    ~PushRelabel();
+
+};
+
+#endif // PUSH_RELABEL_H
---
+++ src/push_relabel.cpp
@@ -0,0 +1,11 @@
+#include "inc/push_relabel.h"
+
+PushRelabel::PushRelabel()
+{
+
+}
+
+PushRelabel::~PushRelabel()
+{
+
+}
