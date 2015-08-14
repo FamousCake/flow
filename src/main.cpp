84a2298ec592f40bb2c1b20165283dfefdbb790e
Famouscake <peter.g.hristov@gmail.com>
2015-08-14 18:45:39 +0300

    formated output

--- src/main.cpp
+++ src/main.cpp
@@ -59,33 +59,33 @@ int main()
     //
     // Stanford
     //
-    S.start("S");
-    auto PR = GetStanfordFlow(E);
-    long long flowS = PR.GetMaxFlow(E.getSource(), E.getSink());
-    S.stop("S");
+    // S.start("S");
+    // auto PR = GetStanfordFlow(E);
+    // long long flowS = PR.GetMaxFlow(E.getSource(), E.getSink());
+    // S.stop("S");
 
     //
     // RESULTS
     //
     cout << "Vertices : " << E.getCount() << endl;
     cout << "Edges    : " << E.getEdgesCount() << endl;
-    cout << "\n";
+    // cout << "\n";
     cout << "RTF Flow : " << RTF.E.getFlow() << endl;
-    cout << "Sta Flow : " << flowS << endl;
+    // cout << "Sta Flow : " << flowS << endl;
     // cout << "FF  Flow : " << FF.E.getFlow() << endl;
-    cout << "\n";
+    // cout << "\n";
     cout << "RTF Time : " << S.get_total_time("RTF") << endl;
-    cout << "Sta Time : " << S.get_total_time("S") << endl;
+    // cout << "Sta Time : " << S.get_total_time("S") << endl;
     // cout << "FF  Time : " << S.get_total_time("FF") << endl;
-    cout << "\n";
-    // cout << "FF Paths : " << FF.IterationsCount << endl;
-    cout << "\n";
-    cout << "Push     : " << RTF.PushCount << endl;
-    cout << "Relabel  : " << RTF.RelabelCount << endl;
-    cout << "Discharge: " << RTF.DischargeCount << endl;
-
-    cout << "\n";
-    cout << "\n";
+    // cout << "\n";
+    // // cout << "FF Paths : " << FF.IterationsCount << endl;
+    // cout << "\n";
+    // cout << "Push     : " << RTF.PushCount << endl;
+    // cout << "Relabel  : " << RTF.RelabelCount << endl;
+    // cout << "Discharge: " << RTF.DischargeCount << endl;
+    //
+    // cout << "\n";
+    // cout << "\n";
 
     return 0;
 }
